dafd500d529859f09828f206ec844c1c45fb580c
John <John@DESKTOP-96UE3Q7>
2023-12-29 19:42:02 -0600

    Add project files.

---
+++ CRUD_EF_Framework/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CRUD_EF_Framework.Models;
+using System.Data.Entity;
+
+namespace CRUD_EF_Framework
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public AppDbContext():base("name=DefaultConnection")
+        {
+            
+        }
+    }
+}
